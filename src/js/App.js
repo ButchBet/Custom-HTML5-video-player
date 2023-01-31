d1badf45a26d04f13473035dc3a1f787c75b0450
ButchBet <butchbet01@gmail.com>
2023-01-31 13:37:59 -0500

    Implemented animation to when the user hover the container of the video and leave it, I need to add it to when the user stop moving the mouse over 5 seconds.

--- src/js/App.js
+++ src/js/App.js
@@ -2,12 +2,44 @@ const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
 const downControls = document.getElementById("downControls");
 
-console.log(videoControls)
+let id = null;
 
-videoContainer.addEventListener("mouseenter", (e) => {
-    videoControls.classList.add("hoverButton");
-});
+videoContainer.addEventListener("mouseenter", showControls);
 
-videoContainer.addEventListener("mouseleave", (e) => {
-    videoControls.classList.remove("hoverButton");
-})
+videoContainer.addEventListener("mouseleave", hiddeControls)
+
+function showControls() {
+    let pos = (-32);
+
+    clearInterval(id);
+
+    id = setInterval(frame, 2);
+
+    function frame(){
+        if(pos == 8) {
+            clearInterval(id);
+        } else {
+            pos++;
+
+            videoControls.style.bottom = pos + "px";
+        }
+    }
+}
+
+function hiddeControls() {
+    let pos = 8;
+
+    clearInterval(id);
+
+    id = setInterval(frame, 2);
+
+    function frame() {
+        if(pos == -32) {
+            clearInterval(id);
+        } else {
+            pos--;
+            
+            videoControls.style.bottom = pos + "px";
+        }
+    }
+}
