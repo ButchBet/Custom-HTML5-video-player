0586634d19c763476dcf46fe49552263acb8711b
ButchBet <butchbet01@gmail.com>
2023-01-31 14:19:59 -0500

    Finished the animation to show and hidde the videoControls.

--- src/js/App.js
+++ src/js/App.js
@@ -2,18 +2,40 @@ const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
 const downControls = document.getElementById("downControls");
 
-let id = null;
+let id = null; // Interval used to show and hidde videoControls
+let checkMovement = false; // Boolean to check if the mouse is being moved or not 
+let hidde = null; // Timeout to hidde the videoControl
+let pos = (-32); // This will help us to move the videoControls naturally
 
 videoContainer.addEventListener("mouseenter", showControls);
 
-videoContainer.addEventListener("mouseleave", hiddeControls)
+videoContainer.addEventListener("mousemove", (e) => {
+    if(checkMovement) {
+        clearTimeout(hidde);
+    
+        hidde = setTimeout(() => {
+            hiddeControls();
 
+            checkMovement = false;
+        }, 2000);
+    } else {
+        showControls();
+
+        checkMovement = true;
+    }
+});
+
+videoContainer.addEventListener("mouseleave", () => {
+    clearTimeout(hidde);
+    
+    hiddeControls();
+})
+
+// Fucntion to how the videoControls slowly
 function showControls() {
-    let pos = (-32);
-
     clearInterval(id);
 
-    id = setInterval(frame, 2);
+    id = setInterval(frame, 1);
 
     function frame(){
         if(pos == 8) {
@@ -26,12 +48,11 @@ function showControls() {
     }
 }
 
+// Function to hidde the videoControls slowly
 function hiddeControls() {
-    let pos = 8;
-
     clearInterval(id);
 
-    id = setInterval(frame, 2);
+    id = setInterval(frame, 1);
 
     function frame() {
         if(pos == -32) {
