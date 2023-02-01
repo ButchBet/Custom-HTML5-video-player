78e6e291a5bea4d1af714368c40248466fb71c2d
Butch Bet <butchbet01@gmail.com>
2023-02-01 14:00:59 -0500

    Made some changes in the animation of the videoControls

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,14 @@
 const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
-const downControls = document.getElementById("downControls");
 
+// Code to animate the show and hidde of the videoControls
 let id = null; // Interval used to show and hidde videoControls
 let checkMovement = false; // Boolean to check if the mouse is being moved or not 
 let hidde = null; // Timeout to hidde the videoControl
-let pos = (-21); // This will help us to move the videoControls naturally
+
+// let pos = (-21); // This will help us to move the videoControls naturally
+let pos = null;// This will help us to move the videoControls naturally
+setBottomPosition();
 
 videoContainer.addEventListener("mouseenter", showControls);
 
@@ -29,10 +32,13 @@ videoContainer.addEventListener("mouseleave", () => {
     clearTimeout(hidde);
     
     hiddeControls();
-})
+});
+
+window.addEventListener("resize", setBottomPosition);
 
 // Fucntion to how the videoControls slowly
 function showControls() {
+    console.log(pos)
     clearInterval(id);
 
     id = setInterval(frame, 1);
@@ -55,7 +61,9 @@ function hiddeControls() {
     id = setInterval(frame, 1);
 
     function frame() {
-        if(pos == -21) {
+        if(pos == -37) {
+            clearInterval(id);
+        } else if(pos == -21 && window.outerWidth >= 599) {
             clearInterval(id);
         } else {
             pos--;
@@ -64,3 +72,16 @@ function hiddeControls() {
         }
     }
 }
+
+// Function to set bottom position when the width of the window changes
+function setBottomPosition() {
+    if(window.outerWidth < 599) {
+        pos = (-37); 
+        videoControls.style.bottom = pos + "px";
+    } else {
+        pos = (-21); 
+        videoControls.style.bottom = pos + "px";
+    }
+}
+
+// Code to add the pause and play event in the respective buttons and the space key
